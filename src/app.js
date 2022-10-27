864a980001ed44b86825a54fbb516b2e78066e96
AltuHoff <104897119+AltuHoff@users.noreply.github.com>
2022-10-27 17:50:00 +0300

    add const

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 export const init = () => {
 	// Start here
-
+const BTC = 'Bitcoin ';
+const ETH = 'Ethereum ';
+const EGLD = 'Elrond'
+const cryptos = BTC + ETH + EGLD;
 	// End here
 
 	console.log(`Hello! It's my first app`);
