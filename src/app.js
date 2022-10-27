a1eaab6e276bb830118ae7c33cca2166f1edd34a
SergienkoNikita <SNikita312@yandex.ru>
2022-10-27 18:08:06 +0300

    refactor hw #1

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 export const init = () => {
 	// Start here
-const BTC = 'Bitcoin ';
-const ETH = 'Ethereum ';
-const EGLD = 'Elrond'
-const cryptos = BTC + ETH + EGLD;
+	const cryptos = ['BTC', 'ETH', 'EGLD'];
 	// End here
 
 	console.log(`Hello! It's my first app`);
