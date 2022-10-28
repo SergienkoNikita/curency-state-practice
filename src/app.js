f8f85b14fbf056b0ff68bb73298b99ad377bd29a
AltuHoff <104897119+AltuHoff@users.noreply.github.com>
2022-10-28 16:14:31 +0300

    add switch case

--- src/app.js
+++ src/app.js
@@ -6,7 +6,26 @@ export const init = () => {
 
 	// Start here
 	const cryptos = ['BTC', 'ETH', 'EGLD'];
+
+	switch (countryCode) {
+		case 'UA':
+			alert('Украина');
+			break;
+		case 'RU':
+			alert('Россия');
+			break;
+		case 'KZ':
+			alert('Казахстан');
+			break;
+		case 'BY':
+			alert('Беларусь');
+			break;
+		default:
+			alert('Страна не верна!');
+			alert('Введите одну из следующих стран: UA, RU, KZ, BY.');
+	}
 	// End here
+
 
 	console.log(`Привет! Мы определили что ты из ${userCountry}`);
 	console.log(`Ты можешь посмотреть курсы ${cryptos} к EUR и USD`);
