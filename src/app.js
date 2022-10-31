1e62689c7fab15d4264a34d81ad744b5e1308310
AltuHoff <104897119+AltuHoff@users.noreply.github.com>
2022-10-31 12:39:22 +0300

    replaced switch to else if, add let to previewCrypto && moreCryptos

--- src/app.js
+++ src/app.js
@@ -11,30 +11,28 @@ export const init = () => {
 
 	// Start here
 	let userCountry = 'неизсвестной нам страны!';
+	let previewCryptos = cryptos.slice(0,3);
+	let moreCryptos = cryptos.slice(3,8);
 
-	switch (countryCode) {
-		case 'UA':
-			userCountry = 'Украина';
-			break;
-		case 'RU':
-			userCountry =  'Россия';
-			break;
-		case 'KZ':
-			userCountry =  'Казахстан';
-			break;
-		case 'BY':
-			userCountry =  'Беларусь';
-			break;
+	if (countryCode === 'UA') {
+		userCountry = 'Украина';
+	} else if (countryCode === 'RU') {
+		userCountry = 'Россия';
+	} else if (countryCode === 'KZ') {
+		userCountry = 'Казахстан';
+	} else if (countryCode === 'BY') {
+		userCountry = 'Беларусь';
 	}
 
 	// End here
 
-	// Говорим пользователю из какой он страны
-	console.log(`Привет! Мы определили что ты из ${userCountry}`);
-	// Говорим 3 самые популярные криптовалюты
-	console.log(`Ты можешь посмотреть курсы ${previewCryptos} к EUR и USD`);
-	// Показываем чуть еще 5 криптовалют
-	console.log(`Так-же доступны:  ${moreCryptos}`);
-	// Отображаем пользователю все доступные для него криптовалюты
-	console.log(`Все доступные:  ${cryptos}`);
+		// Говорим пользователю из какой он страны
+		console.log(`Привет! Мы определили что ты из ${userCountry}`);
+		// Говорим 3 самые популярные криптовалюты
+		console.log(`Ты можешь посмотреть курсы ${previewCryptos} к EUR и USD`);
+		// Показываем чуть еще 5 криптовалют
+		console.log(`Так-же доступны:  ${moreCryptos}`);
+		// Отображаем пользователю все доступные для него криптовалюты
+		console.log(`Все доступные:  ${cryptos}`);
 }
+
