dc3fbb4d9a224f5644ad45469f7a1d90db1ecafa
AltuHoff <104897119+AltuHoff@users.noreply.github.com>
2022-11-19 16:58:29 +0300

    add 1-2

--- src/crypto-app/app.js
+++ src/crypto-app/app.js
@@ -1,30 +1,25 @@
-import { getRandCountryCode } from "../mocks/mocks.js";
+import readlineSync from "readline-sync";
+import {getRandCountryCode} from "../mocks/mocks.js";
 import {getGryptos} from "./api.js";
+import {firstCharToUpperCase} from "../games/utils.js";
 
-function getCountryName (arg) {
-	if (arg === 'UA') {
-		return 'Украина';
-	}
-	if (arg === 'RU') {
-		return 'Россия';
-	}
-	if (arg === 'KZ') {
-		return 'Казахстан';
-	}
-	if (arg === 'BY') {
-		return 'Беларусь';
-	}
-	return 'неизсвестной нам страны'
-}
 
-// Задача 2
-function getCryptosPreview (arg) {
-	return arg.slice(0, 3);
-}
+// function getCountryName (arg) {
+// 	if (arg === 'UA') {
+// 		return 'Украина';
+// 	}
+// 	if (arg === 'RU') {
+// 		return 'Россия';
+// 	}
+// 	if (arg === 'KZ') {
+// 		return 'Казахстан';
+// 	}
+// 	if (arg === 'BY') {
+// 		return 'Беларусь';
+// 	}
+// 	return 'неизсвестной нам страны'
+// }
 
-function getMoreCryptos (arg) {
-	return arg.slice(3, 8);
-}
 
 
 /**
@@ -45,22 +40,68 @@ function objClone(targetObj = {}, titleObj) {
 export async function cryptoApp() {
 	// Код страны
 	const countryCode = getRandCountryCode();
-	// Авторизован ли пользователь
-	const isAuthorize = !!Math.floor(Math.random() * 2);
+
 	// Все доступные криптовалюты
 	const cryptos = await getGryptos()
 
-	if (!isAuthorize) {
-		cryptos.splice(0,cryptos.length / 2)
-	}
+	// if (isAuthorize) {
+	// 	cryptos.splice(0,cryptos.length / 2)
+	// }
 
-	let previewCryptos = getCryptosPreview(cryptos);
-	let moreCryptos = getMoreCryptos(cryptos);
-	let userCountry = getCountryName(countryCode);
+	// let previewCryptos = getCryptosPreview(cryptos);
+	// let moreCryptos = getMoreCryptos(cryptos);
+	// let userCountry = getCountryName(countryCode);
+
 
 	// Start here
 
+		function User ({name, eMail}) {
+		this.name = name;
+		this.eMail = eMail;
+	}
 
+	function checkIsUserWantDoRegistration () {
+		let questionToUser = firstCharToUpperCase(readlineSync.question('Do you want to register?\nAnswer "Yes" or "No". '));
+		return questionToUser === 'Yes'? true : console.log(' Sorry, you have canceled your registration.');
+	}
+	function getUserData () {
+		if (checkIsUserWantDoRegistration() === true) {
+			let name = readlineSync.question('Write your Name. ')
+			let eMail = readlineSync.question('Write your eMail. ')
+			return [{name: name, eMail: eMail}] ;
+		}
+	}
+
+	const createdUserFromUserData = getUserData().map((userData) => new User(userData))
+	console.log(createdUserFromUserData)
+
+	// function isAuthorise (createdUserFromUserData) {
+	// 	if (createdUserFromUserData) {
+	//
+	// 	}
+	// }
+	// isAuthorise(createdUserFromUserData);
+
+	let isAuthorise = false;
+	if (!!createdUserFromUserData) isAuthorise = true;
+
+
+
+	console.log(Boolean(isAuthorise))
+
+
+
+
+// function getCryptosPreview (arg) {
+// 	return arg.slice(0, 3);
+// }
+//
+// function getMoreCryptos (arg) {
+// 	return arg.slice(3, 8);
+// }
+// if (isAuthorize) {
+// 	cryptos.splice(0,cryptos.length / 2)
+// }
 
 
 	// // End here
