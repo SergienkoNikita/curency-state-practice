fb530d180bf4dc2975c68fec4e12275aed06da18
SergienkoNikita <SNikita312@yandex.ru>
2022-11-23 11:37:45 +0300

    refactor hw-10 text

--- src/crypto-app/app.js
+++ src/crypto-app/app.js
@@ -1,118 +1,39 @@
-import readlineSync from "readline-sync";
-import {getRandCountryCode} from "../mocks/mocks.js";
 import {getGryptos} from "./api.js";
-import {firstCharToUpperCase} from "../games/utils.js";
-
-
-// function getCountryName (arg) {
-// 	if (arg === 'UA') {
-// 		return 'Украина';
-// 	}
-// 	if (arg === 'RU') {
-// 		return 'Россия';
-// 	}
-// 	if (arg === 'KZ') {
-// 		return 'Казахстан';
-// 	}
-// 	if (arg === 'BY') {
-// 		return 'Беларусь';
-// 	}
-// 	return 'неизсвестной нам страны'
-// }
-
-
-
-/**
- * @param targetObj{Object} целевой (в который клонируем)
- * @param titleObj{Object} - эталонный из которого клонируем
- * @returns {Object} - Возвращает целевой объект
- */
-function objClone(targetObj = {}, titleObj) {
-	for (let key in titleObj) {
-		targetObj[key] = titleObj[key];
-	}
-
-	return targetObj;
-}
-
+import {askForRegistration, askUserData} from "./user-module/dialog-with-user.js";
+import {User} from "./user-module/user-model.js";
+import {getUserCountry} from "./location/user-location.js";
 
 
 export async function cryptoApp() {
-	// Код страны
-	const countryCode = getRandCountryCode();
+	const cryptos = await getGryptos();
+	Object.keys(cryptos).forEach((cryptoKey) => {
+		cryptos[cryptoKey].USD = cryptos[cryptoKey].USD.toFixed(4)
+	})
+	const wantRegister = askForRegistration();
+	const currentUser = wantRegister ? new User(askUserData()) : new User({name: 'Anonymous'});
+	const isAuthorise = currentUser.name !== 'Anonymous';
+	const cryptosNames = Object.keys(cryptos);
 
-	// Все доступные криптовалюты
-	const cryptos = await getGryptos()
-
-	// if (isAuthorize) {
-	// 	cryptos.splice(0,cryptos.length / 2)
-	// }
-
-	// let previewCryptos = getCryptosPreview(cryptos);
-	// let moreCryptos = getMoreCryptos(cryptos);
-	// let userCountry = getCountryName(countryCode);
-
-
-	// Start here
-
-		function User ({name, eMail}) {
-		this.name = name;
-		this.eMail = eMail;
+	if (!isAuthorise) {
+		cryptosNames.splice(0, cryptosNames.length / 2)
 	}
 
-	function checkIsUserWantDoRegistration () {
-		let questionToUser = firstCharToUpperCase(readlineSync.question('Do you want to register?\nAnswer "Yes" or "No". '));
-		return questionToUser === 'Yes'? true : console.log(' Sorry, you have canceled your registration.');
-	}
-	function getUserData () {
-		if (checkIsUserWantDoRegistration() === true) {
-			let name = readlineSync.question('Write your Name. ')
-			let eMail = readlineSync.question('Write your eMail. ')
-			return [{name: name, eMail: eMail}] ;
-		}
+	let previewCryptos = getCryptosPreview(cryptosNames);
+	let moreCryptos = getMoreCryptos(cryptosNames);
+
+
+	function getCryptosPreview(arg) {
+		return arg.slice(0, 3);
 	}
 
-	const createdUserFromUserData = getUserData().map((userData) => new User(userData))
-	console.log(createdUserFromUserData)
-
-	// function isAuthorise (createdUserFromUserData) {
-	// 	if (createdUserFromUserData) {
-	//
-	// 	}
-	// }
-	// isAuthorise(createdUserFromUserData);
-
-	let isAuthorise = false;
-	if (!!createdUserFromUserData) isAuthorise = true;
-
-
-
-	console.log(Boolean(isAuthorise))
-
-
-
-
-// function getCryptosPreview (arg) {
-// 	return arg.slice(0, 3);
-// }
-//
-// function getMoreCryptos (arg) {
-// 	return arg.slice(3, 8);
-// }
-// if (isAuthorize) {
-// 	cryptos.splice(0,cryptos.length / 2)
-// }
-
-
-	// // End here
-	//
-	// // Говорим пользователю из какой он страны
-	// console.log(`Привет! Мы определили что ты из ${userCountry}`);
-	// // Говорим 3 самые популярные криптовалюты
-	// console.log(`Ты можешь посмотреть курсы ${previewCryptos} к EUR и USD`);
-	// // Показываем чуть еще 5 криптовалют
-	// console.log(`Так-же доступны:  ${moreCryptos}`);
-	// // Отображаем пользователю все доступные для него криптовалюты
-	// console.log(`Все доступные:  ${cryptos}`);
+	function getMoreCryptos(arg) {
+		return arg.slice(3, 8);
 	}
+
+
+	console.log(`Привет! Мы определили что ты из ${getUserCountry()}`);
+	console.log(`Ты можешь посмотреть курсы ${previewCryptos} к EUR и USD`);
+	console.log(`Так-же доступны:  ${moreCryptos}`);
+	console.log(`Все доступные:  ${cryptosNames}`);
+}
 
---
+++ src/crypto-app/user-module/dialog-with-user.js
@@ -0,0 +1,32 @@
+import readlineSync from "readline-sync";
+
+import {YES_NO_ANSWER_STRING_VARIANTS} from "../../games/is-even-game/constants.js";
+
+export function askForRegistration() {
+	const userAnswer = readlineSync.question('Do you want to register?\nAnswer "Yes" or "No". ').toLowerCase();
+
+	if (!Object.values(YES_NO_ANSWER_STRING_VARIANTS).includes(userAnswer)) {
+		console.log('Your answer must be "Yes" or "No" \nTry again!');
+		return askForRegistration();
+	}
+
+	if (userAnswer !== YES_NO_ANSWER_STRING_VARIANTS.YES) console.log('You have canceled your registration.');
+
+	return userAnswer === YES_NO_ANSWER_STRING_VARIANTS.YES
+}
+
+export function askUserData () {
+	const userData = {
+		name: readlineSync.question('Write your name: '),
+	}
+
+	if (!userData.name) {
+		console.log('"Name" must be required!');
+		return askUserData();
+	}
+
+	return {
+		...userData,
+		email: readlineSync.question('Write your email: '),
+	}
+}
