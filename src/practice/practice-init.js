ef9d196c2adb091a2ce3b13ad71cbafa01f0a78e
AltuHoff <104897119+AltuHoff@users.noreply.github.com>
2022-11-24 14:14:50 +0300

    create 3 game

---
+++ src/practice/practice-init.js
@@ -0,0 +1,44 @@
+// hw 12.1
+const someNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+function createPhoneNumber (arr) {
+  return `(${arr[0]}${arr[1]}${arr[2]}) ${arr[3]}${arr[4]}${arr[5]}-${arr[6]}${arr[7]}-${arr[8]}${arr[9]} `;
+}
+console.log(createPhoneNumber(someNumber));
+// hw 12.2
+const arrayWalk = ['n', 's', 'w', 'e', 'n', 's', 'w', 'e', 'n', 's'];
+
+function findPosition (arr) {
+  if (arr.length !== 10) return false;
+
+  let n = 0;
+  let s = 0;
+  let w = 0;
+  let e = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] === 'n') n += 1;
+    if (arr[i] === 's') s += 1;
+    if (arr[i] === 'w') w += 1;
+    if (arr[i] === 'e') e += 1;
+  }
+  if (n !== s && w !== e) return false;
+  if (n === s && w === e) return true;
+}
+
+console.log(findPosition(arrayWalk))
+
+// hw 12.3
+const arr1 = [];
+const arr2 = ["Peter"]
+const arr3 = ["Jacob", "Alex"]
+const arr4 = ["Max", "John", "Mark"]
+const arr5 = ["Alex", "Jacob", "Mark", "Max"]
+
+function whoLike (arr) {
+  if (arr[0] === undefined) return `no one likes this`;
+  if (arr.length < 2) return `${arr} likes this`;
+  if (arr.length === 2) return `${arr[0]} and ${arr[1]} likes this`;
+  if (arr.length === 3) return `${arr[0]}, ${arr[1]} and ${arr[2]} likes this`;
+  if (arr.length > 3)  return `${arr[0]}, ${arr[1]} and ${arr.length - 2} other likes this`;
+}
+
+console.log(whoLike(arr3))
