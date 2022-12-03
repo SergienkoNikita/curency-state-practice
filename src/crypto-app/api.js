2b709ae9d9d578cd8805b9cc2503e5c140c398dd
SergienkoNikita <SNikita312@yandex.ru>
2022-12-03 16:15:29 +0300

    fix hw text

--- src/crypto-app/api.js
+++ src/crypto-app/api.js
@@ -1,9 +1,7 @@
-import fetch from "node-fetch";
-
+import fetch from 'node-fetch';
 
 export async function getGryptos() {
-	return fetch('https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH,BNB,XRP,DOGE,MATIC,USDT,BUSD,LTC,MASK&tsyms=USD&api_key=e64344a825c5c3552147fedcb5baa1fcc34005cc85a1772321984ee62ce7b7c1')
-		.then((res) => res.json())
-		.then((json) => {
-			return json})
+  return fetch('https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH,BNB,XRP,DOGE,MATIC,USDT,BUSD,LTC,MASK&tsyms=USD&api_key=e64344a825c5c3552147fedcb5baa1fcc34005cc85a1772321984ee62ce7b7c1')
+    .then((res) => res.json())
+    .then((json) => json);
 }
